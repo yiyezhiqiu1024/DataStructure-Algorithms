dc20bb68d70dc8fd2b09736e598f187466dcd87b
CoderSLZeng <zeng@AmassdeMac-mini.local>
2019-05-11 11:29:37 +0800

    add 05-队列 CircleQueue 循环队列

--- Java/05-队列/src/com/zeng/Main.java
+++ Java/05-队列/src/com/zeng/Main.java
@@ -1,5 +1,7 @@
 package com.zeng;
 
+import com.zeng.circle.CircleQueue;
+
 public class Main {
 	
 	static void testQueue() {
@@ -28,10 +30,40 @@ public class Main {
 			System.out.println(deque.deQueueRear());
 		}
 	}
+	
+	static void testCircleQueue() {
+		CircleQueue<Integer> queue = new CircleQueue<>();
+		// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+		for (int i = 0; i < 10; i++) {
+			queue.enQueue(i);
+		}
+		
+		System.out.println(queue); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		
+		for (int i = 0; i < 5; i++) {
+			queue.deQueue();
+		}
+		
+		System.out.println(queue); // [null, null, null, null, null, 5, 6, 7, 8, 9]
+		
+		// 15, 16, 17, 18, 19, 5, 6, 7, 8, 9
+		for (int i = 15; i < 20; i++) {
+			queue.enQueue(i);
+		}
+		
+		System.out.println(queue); // [15, 16, 17, 18, 19, 5, 6, 7, 8, 9]
+		
+		while (!queue.isEmpty()) {
+			System.out.println(queue.deQueue());
+		}
+		
+		System.out.println(queue);
+	}
 
 	public static void main(String[] args) {
 //		testQueue();
-		testDeque();
+//		testDeque();
+		testCircleQueue();
 	}
 
 }
