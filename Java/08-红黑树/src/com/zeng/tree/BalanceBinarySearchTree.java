6396666c1445258912185161614e46161907b2d0
CoderSLZeng <zeng@AmassdeMac-mini.local>
2019-05-22 14:03:42 +0800

    add 08-红黑树

---
+++ Java/08-红黑树/src/com/zeng/tree/BalanceBinarySearchTree.java
@@ -0,0 +1,83 @@
+package com.zeng.tree;
+
+import java.util.Comparator;
+
+public class BalanceBinarySearchTree<E> extends BinarySearchTree<E> {
+
+	public BalanceBinarySearchTree() {
+		this(null);
+	}
+
+	public BalanceBinarySearchTree(Comparator<E> comparator) {
+		super(comparator);
+	}
+	
+	protected void rotateLeft(Node<E> grand) {
+		Node<E> parent = grand.right;
+		Node<E> child = parent.left;
+		grand.right = child;
+		parent.left = grand;
+		afterRotate(grand, parent, child);
+	}
+
+	protected void rotateRight(Node<E> grand) {
+		Node<E> parent = grand.left;
+		Node<E> child = parent.right;
+		grand.left = child;
+		parent.right = grand;
+		afterRotate(grand, parent, child);
+	}
+	
+	protected void afterRotate(Node<E> grand, Node<E> parent, Node<E> child) {
+		// 让parent成为子树的根节点
+		parent.parent = grand.parent;
+		if (grand.isLeftChild()) {
+			grand.parent.left = parent;
+		} else if (grand.isRightChild()) {
+			grand.parent.right = parent;
+		} else { // grand是root节点
+			root = parent;
+		}
+		
+		// 更新child的parent
+		if (child != null) {
+			child.parent = grand;
+		}
+		
+		// 更新grand的parent
+		grand.parent = parent;
+	}
+	
+	protected void rotate(
+			Node<E> r, // 子树的根节点 
+			Node<E> b, Node<E> c,
+			Node<E> d,
+			Node<E> e, Node<E> f) {
+		// 让d成为这颗子树的根节点
+		d.parent = r.parent;
+		if (r.isLeftChild()) {
+			r.parent.left = d;
+		} else if (r.isRightChild()) {
+			r.parent.right = d;
+		} else {
+			root = d;
+		}
+		
+		// b-c
+		b.right = c;
+		if (c != null) 
+			c.parent = b;
+		
+		// e-f
+		f.left = e;
+		if (e != null) 
+			e.parent = f;
+
+		// b-d-f
+		d.left = b;
+		d.right = f;
+		b.parent = d;
+		f.parent = d;
+	}
+	
+}
---
+++ Java/08-红黑树/src/com/zeng/tree/BinaryTree.java
@@ -0,0 +1,297 @@
+package com.zeng.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.zeng.printer.BinaryTreeInfo;
+
+@SuppressWarnings("unchecked")
+public class BinaryTree<E> implements BinaryTreeInfo {
+	
+	protected int size;
+	protected Node<E> root;
+	
+	/**
+	 * @return 元素的数量
+	 */
+	public int size() {
+		return size;
+	}
+	
+	/**
+	 * @return 是否为空
+	 */
+	public boolean isEmpty() {
+		return 0 == size;
+	}
+	
+	/**
+	 * 清空所有元素
+	 */
+	public void clear() {
+		root = null;
+		size = 0;
+	}
+	
+	/**
+	 * 前序遍历
+	 * @param visitor
+	 */
+	public void preorder(Visitor<E> visitor) {
+		preorder(root, visitor);
+	}
+
+	private void preorder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor == null) return;
+		
+		visitor.visitor(node.element);
+		preorder(node.left, visitor);
+		preorder(node.right, visitor);
+	}
+	
+	/**
+	 * 中序遍历
+	 * @param visitor
+	 */
+	public void inorder(Visitor<E> visitor) {
+		inorder(root, visitor);
+	}
+	
+	private void inorder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor == null) return;
+		inorder(node.left, visitor);
+		visitor.visitor(node.element);
+		inorder(node.right, visitor);
+	}
+	
+	/**
+	 * 后序遍历
+	 * @param visitor
+	 */
+	public void postorder(Visitor<E> visitor) {
+		postorder(root, visitor);
+	}
+
+	private void postorder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor == null) return;
+		
+		postorder(node.left, visitor);
+		postorder(node.right, visitor);
+		visitor.visitor(node.element);
+	}
+	
+	/**
+	 * 层序遍历
+	 * @param visitor
+	 */
+	public void levelOrder(Visitor<E> visitor) {
+		if (root == null || visitor == null) return;
+		
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while (!queue.isEmpty()) {
+		 	Node<E> node = queue.poll();
+			visitor.visitor(node.element);
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+	}
+	
+	/**
+	 * @return 计算一个树是否为完全二叉树
+	 */
+	public boolean isComplete() {
+		if (root == null) return false;
+		
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		boolean leaf = false;
+		while (!queue.isEmpty()) {
+			Node<E> node = queue.poll();
+			if (leaf && !node.isLeaf()) return false;
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			} else if (node.right != null) { // node.left == null && node.right != null
+				return false;
+			} 
+
+			if (node.right != null) { // node.left != null && node.right != null
+				queue.offer(node.right);
+			} else { // 后面遍历的节点都必须是叶子节点
+				leaf = true;
+			}
+		}
+		
+		return leaf;
+	}
+	
+	/**
+	 * 计算二叉树的高度（迭代）
+	 */
+	public int height() {
+		if (root == null) return 0;
+		// 数的高度
+		int height = 0;
+		// 存储着每一层的元素数量
+		int levelSize = 1;
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while (!queue.isEmpty()) {
+			Node<E> node = queue.poll();
+			levelSize--;
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+			
+			if (0 == levelSize) {
+				levelSize = queue.size();
+				height++;
+			}
+		}
+		
+		return height;
+	}
+	
+	/**
+	 * 计算二叉树的高度（递归）
+	 */
+	public int height2() {
+		return height(root);
+	}
+	
+	/**
+	 * 计算二叉树的高度（递归）
+	 * @param node
+	 * @return 二叉树的高度
+	 */
+	public int height(Node<E> node) {
+		if (node == null) return 0;
+		return 1 + Math.max(height(node.left), height(node.right));
+	}
+	
+	protected Node<E> createNode(E element, Node<E> parent) {
+		return new Node<>(element, parent);
+	}
+	
+	/**
+	 * @param node
+	 * @return 前驱节点
+	 */
+	protected Node<E> predecessor(Node<E> node) {
+		if (node == null) return null;
+		
+		// 前驱节点在左子树当中（left.right.right.right...）
+		Node<E> p = node.left;
+		if (p != null) {
+			while (p.right != null) {
+				p = p.right;
+			}
+			return p;
+		}
+		
+		// 从父节点、祖父节点中寻找前驱节点
+		while (node.parent != null && node == node.parent.left) {
+			node = node.parent;
+		}
+		
+		// node.parent == null
+		// node == node.parent.right
+		return node.parent;
+	}
+	
+	protected Node<E> successor(Node<E> node) {
+		if (node == null) return null;
+		// 后继节点在左子树当中（right.left.left.left...）
+		Node<E> s = node.right;
+		if (s != null) {
+			while (s.left != null) {
+				s = s.left;
+			}
+			return s;
+		}
+		
+		// 从父节点、祖父节点中寻找后继节点
+		while (node.parent != null && node == node.parent.right) {
+			node = node.parent;
+		}
+		
+		return node.parent;
+	}
+	
+	@Override
+	public Object root() {
+		return root;
+	}
+
+	@Override
+	public Object left(Object node) {
+		return ((Node<E>)node).left;
+	}
+
+	@Override
+	public Object right(Object node) {
+		return ((Node<E>)node).right;
+	}
+
+	@Override
+	public Object string(Object node) {
+		return node;
+	}
+	
+	public static interface Visitor<E> {
+		void visitor(E element);
+	}
+	
+	@SuppressWarnings("unused")
+	protected static class Node<E> {
+		E element;
+		Node<E> left;
+		Node<E> right;
+		Node<E> parent;
+		
+		public Node(E element, Node<E> parent) {
+			this.element = element;
+			this.parent = parent;
+		} 
+		
+		public boolean isLeaf() {
+			return left == null && right == null;
+		}
+		
+		public boolean hasTowChildren() {
+			return left != null && right != null;
+		}
+		
+		public boolean isLeftChild() {
+			return parent != null && this == parent.left;
+		}
+		
+		public boolean isRightChild() {
+			return parent != null && this == parent.right;
+		}
+
+		public Node<E> sibling() {
+			if (isLeftChild()) return parent.right;
+			
+			if (isRightChild()) return parent.left;
+			
+			return null;
+		}
+	}
+
+}
