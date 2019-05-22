eb06e7ed1d4068b39a34bde5f87a7aebae9e2a6b
CoderSLZeng <zeng@AmassdeMac-mini.local>
2019-05-22 17:06:14 +0800

    add 08-红黑树 删除节点

--- Java/08-红黑树/src/com/zeng/Main.java
+++ Java/08-红黑树/src/com/zeng/Main.java
@@ -79,9 +79,30 @@ public class Main {
 		BinaryTrees.println(rb);
 	}
 	
+	static void test4() {
+		Integer data[] = new Integer[] {
+				55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50
+		};
+		
+		RBTree<Integer> rb = new RBTree<>();
+		for (int i = 0; i < data.length; i++) {
+			rb.add(data[i]);
+		}
+
+		BinaryTrees.println(rb);
+
+		for (int i = 0; i < data.length; i++) {
+			rb.remove(data[i]);
+			System.out.println("---------------------------------------");
+			System.out.println("【" + data[i] + "】");
+			BinaryTrees.println(rb);
+		}
+	}
+	
 	public static void main(String[] args) {
 //		test1();
 //		test2();
-		test3();
+//		test3();
+		test4();
 	}
 }
